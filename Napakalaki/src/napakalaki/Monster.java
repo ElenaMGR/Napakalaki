a8a8993c1d0d4327344913e2583fbebfae0acd4e
joselmo <joselmo@correo.ugr.es>
2015-10-05 18:12:21 +0200

    Creada la clase Monster. Cambiado el autor por defecto de Netbeans

---
+++ Napakalaki/src/napakalaki/Monster.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author Elena María Gómez Ríos, Jose Luis Martínez Ortiz
+ */
+public class Monster {
+    // Variables 
+    private String name;
+    private int combatLevel;
+    
+}
