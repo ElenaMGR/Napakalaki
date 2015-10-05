60aa107b62b882c5648b2ce0395efbc55d2d5ed6
ElenaMGR <elenamgori@gmail.com>
2015-10-05 11:41:34 +0200

    Creación de la clase Prize

---
+++ Napakalaki/src/napakalaki/Prize.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author Elena María Gómez Ríos, Jose Luis Martínez Ortiz
+ */
+public class Prize {
+    private int treasures;
+    private int level;
+    //Constructor
+    Prize(int treasures, int level){
+        this.treasures=treasures;
+        this.level=level;
+    }
+    //Métodos get
+    public int getTreasures(){
+        return treasures;
+    }
+    public int getLevel(){
+        return level;
+    }
+    
+}
