2d27eae44dd2b5203f6f085ee048e3c360fe7f30
joselmo <joselmo@correo.ugr.es>
2015-10-05 18:34:26 +0200

    Modificado el constructor de Monster. Sobreescrito el metodo toString()

--- Napakalaki/src/napakalaki/Monster.java
+++ Napakalaki/src/napakalaki/Monster.java
@@ -12,23 +12,32 @@ package napakalaki;
 public class Monster {
 
     // Variables 
-
     private String name;
     private int combatLevel;
     private BadConsequence bc;
     private Prize price;
 
     //Constructor
-    public Monster(String name, int level, BadConsequence bc, Prize price) {    
+    public Monster(String name, int level, BadConsequence bc, Prize price) {
         this.name = name;
         this.combatLevel = combatLevel;
+        this.bc = bc;
+        this.price = price;
     }
 
     //Métodos get
     public String getName() {
         return name;
     }
-    public int getCombatLevel(){
+
+    public int getCombatLevel() {
         return combatLevel;
+    }
+
+    //Métodos
+    @Override
+    public String toString() {
+        return "Name = " + name + " CombatLevel = " + Integer.toString(combatLevel)
+                + " Pride :" + price.toString() + " BadConsequence =" + bc.toString();
     }
 }
