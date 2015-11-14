a981741ad2bf9647804ac1dc763c4c4841b17b3a
joselmo <joselmo@correo.ugr.es>
2015-11-14 15:03:00 +0100

    Mod BadConsequence: Actualizados metodos y contructores.

--- Napakalaki/src/napakalaki/BadConsequence.java
+++ Napakalaki/src/napakalaki/BadConsequence.java
@@ -12,26 +12,30 @@ import java.util.ArrayList;
  * @author Elena María Gómez Ríos, Jose Luis Martínez Ortiz
  */
 public class BadConsequence {
+    
+    static final int MAXTREASURES = 10;
+    
     private String text;            //Representa lo que dice un mal rollo
     private int levels;             //Representa los niveles que se pierden    
     private int nVisibleTreasures;  //Representa el número de tesoros visibles que se pierden
     private int nHiddenTreasures;   //Representa el número de tesoros ocultos que se pierden
     private boolean death;          //Representa un mal rollo de tipo muerte
-    private ArrayList<TreasureKind> specificHiddenTreasures = new ArrayList();
-    private ArrayList<TreasureKind> specificVisibleTreasures = new ArrayList();
+    
+    private ArrayList<TreasureKind> specificHiddenTreasures;
+    private ArrayList<TreasureKind> specificVisibleTreasures;
     
     //Constructores
-    public BadConsequence(String text, int levels, int nVisible, int nHidden){
-        this.text=text;
-        this.levels=levels;
+    public BadConsequence(String t, int l, int nVisible, int nHidden){
+        text=t;
+        levels=l;
         nVisibleTreasures=nVisible;
         nHiddenTreasures=nHidden;
         death=false;
         specificHiddenTreasures=new ArrayList();
         specificVisibleTreasures=new ArrayList();
     }
-    public BadConsequence(String text, boolean death){
-        this.text=text;
+    public BadConsequence(String t, boolean death){
+        text=t;
         this.death=death;
         specificHiddenTreasures=new ArrayList();
         specificVisibleTreasures=new ArrayList();
@@ -39,37 +43,37 @@ public class BadConsequence {
         nHiddenTreasures=0;
         levels=0;
     }
-    public BadConsequence(String text, int levels, ArrayList<TreasureKind> tVisible,
-            ArrayList<TreasureKind> tHidden){
-        this.text=text;
-        this.levels=levels;
-        specificHiddenTreasures=tHidden;
-        specificVisibleTreasures=tVisible;
+    public BadConsequence(String t, int l, ArrayList<TreasureKind> v,
+            ArrayList<TreasureKind> h){
+        text=t;
+        levels=l;
+        specificHiddenTreasures=h;
+        specificVisibleTreasures=v;
         nVisibleTreasures=0;
         nHiddenTreasures=0;
         death=false;
     }
 
     //Métodos Get
-    public String getText() {
+    /*public String getText() {
         return text;
-    }
+    }*/
 
     public int getLevels() {
         return levels;
     }
 
-    public int getnVisibleTreasures() {
+    public int getNVisibleTreasures() {
         return nVisibleTreasures;
     }
 
-    public int getnHiddenTreasures() {
+    public int getNHiddenTreasures() {
         return nHiddenTreasures;
     }
 
-    public boolean isDeath() {
-        return death;
-    }
+//    public boolean isDeath() {
+//        return death;
+//    }
     
     public ArrayList<TreasureKind> getSpecificHiddenTreasures(){
         return specificHiddenTreasures;
@@ -77,6 +81,20 @@ public class BadConsequence {
     
     public ArrayList<TreasureKind> getSpecificVisibleTreasures(){
         return specificVisibleTreasures;
+    }
+    
+    public void substracVisibleTreasure(Treasure t){}
+    
+    public void substracHiddenTreasure(Treasure t){}
+    
+    public boolean isEmpty(){
+        return false;
+    }
+    
+    public BadConsequence adjustToFitTreasureList(ArrayList<Treasure> v,ArrayList<Treasure> h){
+        
+        
+        return null;
     }
     
     //Método que muestra el estado de BadConsequence
