b8a7c1e20820f5cd9b355b6aa6646ef8e2cd03da
joselmo <joselmo@correo.ugr.es>
2015-11-14 15:11:11 +0100

    Añadida la clase Treasure

--- Napakalaki/src/napakalaki/Treasure.java
+++ Napakalaki/src/napakalaki/Treasure.java
@@ -10,5 +10,30 @@ package napakalaki;
  * @author Elena María Gómez Ríos, Jose Luis Martínez Ortiz
  */
 class Treasure {
+    private String name;
+    private int bonus;
+    
+    private TreasureKind type;
+    
+    // Constructores
+    public Treasure(String n, int bonus,TreasureKind t){
+        name        = n;
+        this.bonus  = bonus;
+        type        = t;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getBonus(){
+        return bonus;
+    }
+    
+    public TreasureKind getType(){
+        return type;
+    }
+    
+    
     
 }
